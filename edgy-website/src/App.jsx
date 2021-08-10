4a8b5257eba78122386651d6f1fd64288d5e8bbf
Daniel Cabral <1danielcabral@gmail.com>
2021-08-11 00:15:04 +0100

    Started Home Page / Removed excess files
    Added fonts
    Added images
    Removed excess starting project files
    Started implementing Home Screen layout

--- edgy-website/src/App.jsx
+++ edgy-website/src/App.jsx
@@ -1,12 +1,67 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./global.css";
+import centerImage from "./images/tempimage.png";
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <>
+      <div style={divAlignItems}>
+        <label style={edgyTitle}>Edgy</label>
+
+        <div style={divAlignCenterItems}>
+          <img src={centerImage} style={centerImageStyle}></img>
+          <label style={sloganPart1}>edgy is the</label>
+          <label style={sloganPart2}>new normal</label>
+        </div>
+      </div>
+    </>
   );
 }
+
+const divAlignItems = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+};
+
+const divAlignCenterItems = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+};
+
+const edgyTitle = {
+  fontFamily: "Jacksilver",
+  color: "White",
+  fontSize: 40,
+  marginTop: 20,
+  alignSelf: "center",
+};
+
+const centerImageStyle = {
+  alignSelf: "center",
+};
+
+const sloganPart1 = {
+  fontFamily: "Righteous",
+  color: "White",
+  fontSize: 170,
+  position: "absolute",
+  alignSelf: "flex-start",
+  marginTop: -140,
+  marginLeft: 40,
+};
+
+const sloganPart2 = {
+  fontFamily: "Righteous",
+  color: "White",
+  fontSize: 170,
+  position: "absolute",
+  alignSelf: "flex-end",
+  marginBottom: -140,
+  marginRight: 40,
+};
 
 export default App;
