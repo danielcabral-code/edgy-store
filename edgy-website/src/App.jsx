5b4574509ebed73d3de979d559e502d37047e274
Daniel Cabral <1danielcabral@gmail.com>
2021-08-11 22:55:57 +0100

    Home Page Completed / Started Products Page
    Installed styled-components + code refactor of some styles
    Completed Home Page layout including working Instagram button link + Products Page button
    Disabled all text highlighting + image dragging
    Added images

--- edgy-website/src/App.jsx
+++ edgy-website/src/App.jsx
@@ -1,44 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
 import "./global.css";
+
 import centerImage from "./images/tempimage.png";
+import instagramLogo from "./images/instagramLogo.png";
+import graphicDetailsLines from "./images/graphicDetailsLines.png";
 
 function App() {
+  const [mainPageVisibility, setMainPageVisibility] = useState(true);
+
+  function openProductsPage() {
+    setMainPageVisibility(false);
+  }
+
+  function openHomePage() {
+    setMainPageVisibility(true);
+  }
+
   return (
     <>
-      <div style={divAlignItems}>
-        <label style={edgyTitle}>Edgy</label>
+      <MainDiv>
+        <EdgyTitle onClick={openHomePage}>Edgy</EdgyTitle>
 
-        <div style={divAlignCenterItems}>
-          <img src={centerImage} style={centerImageStyle}></img>
-          <label style={sloganPart1}>edgy is the</label>
-          <label style={sloganPart2}>new normal</label>
-        </div>
-      </div>
+        {mainPageVisibility && (
+          <>
+            <CenterAlignDiv>
+              <img
+                src={centerImage}
+                style={centerImageStyle}
+                draggable={false}
+              ></img>
+              <label style={sloganPart1}>edgy is the</label>
+              <label style={sloganPart2}>new normal</label>
+              <img
+                src={graphicDetailsLines}
+                style={alignGraphicDetailsLines}
+                draggable={false}
+              ></img>
+            </CenterAlignDiv>
+
+            <CenterAlignDiv>
+              <a href={"https://instagram.com"} target={"_blank"}>
+                <InstagramLogo
+                  src={instagramLogo}
+                  draggable={false}
+                ></InstagramLogo>
+              </a>
+              <ProductsButton onClick={openProductsPage}>
+                products
+              </ProductsButton>
+            </CenterAlignDiv>
+          </>
+        )}
+      </MainDiv>
     </>
   );
 }
 
-const divAlignItems = {
-  width: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-};
+const MainDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-const divAlignCenterItems = {
-  width: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-};
+const CenterAlignDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-const edgyTitle = {
-  fontFamily: "Jacksilver",
-  color: "White",
-  fontSize: 40,
-  marginTop: 20,
-  alignSelf: "center",
-};
+const EdgyTitle = styled.label`
+  font-family: Jacksilver;
+  color: white;
+  font-size: 40px;
+  margin-top: 20px;
+  align-self: center;
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.4s;
+    cursor: pointer;
+  }
+`;
 
 const centerImageStyle = {
   alignSelf: "center",
@@ -63,5 +108,40 @@ const sloganPart2 = {
   marginBottom: -140,
   marginRight: 40,
 };
+
+const alignGraphicDetailsLines = {
+  position: "absolute",
+  alignSelf: "center",
+  marginTop: 282,
+  marginLeft: 474,
+};
+
+const InstagramLogo = styled.img`
+  position: absolute;
+  align-self: flex-start;
+  bottom: 0px;
+  margin-bottom: 20px;
+  margin-left: 40px;
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.4s;
+    cursor: pointer;
+  }
+`;
+
+const ProductsButton = styled.label`
+  font-family: Righteous;
+  color: white;
+  font-size: 30px;
+  position: absolute;
+  align-self: center;
+  bottom: 0px;
+  margin-bottom: 20px;
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.4s;
+    cursor: pointer;
+  }
+`;
 
 export default App;
